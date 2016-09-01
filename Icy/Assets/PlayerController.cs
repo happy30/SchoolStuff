83f8c047427b32a7671119b3868e520fa02de469
happy30 <jordivdh@hotmail.com>
2016-09-01 13:12:57 +0200

    FirstCommit

---
+++ Icy/Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float frozenPercent;
+    public bool inFreezer;
+    public float meltFactor;
+    public float freezeFactor;
+
+    public float speed;
+    public float jumpHeight;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        frozenPercent = 100;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(!inFreezer)
+        {
+            frozenPercent -= meltFactor * Time.deltaTime;
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.collider2D.tag == "Freezer")
+        {
+            frozenPercent += 
+        }
+    }
+}
