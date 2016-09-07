2fa211dcbd3c847c4b307e252bf0eef4d5154b70
happy30 <jordivdh@hotmail.com>
2016-09-07 14:48:25 +0200

    Wowie so much

--- Icy/Assets/PlayerController.cs
+++ Icy/Assets/PlayerController.cs
@@ -10,18 +10,34 @@ public class PlayerController : MonoBehaviour {
 
     public float speed;
     public float jumpHeight;
+    float xMovement;
+
+    public Rigidbody2D _rb;
 
 
 
 	// Use this for initialization
 	void Start ()
     {
+        _rb = GetComponent<Rigidbody2D>();
         frozenPercent = 100;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        xMovement = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        _rb.velocity = new Vector2(xMovement, 0);
+        if(Input.GetKey("Z"))
+        {
+            if(isTouching().tag == "Ground")
+            {
+
+            }
+        }
+        
+        
+
 	    if(!inFreezer)
         {
             frozenPercent -= meltFactor * Time.deltaTime;
@@ -30,9 +46,20 @@ public class PlayerController : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.gameObject.tag == "Freezer")
+        if (col.gameObject.tag == "Freezer")
         {
             frozenPercent += freezeFactor * Time.deltaTime;
         }
+
+    }
+
+    public GameObject isTouching()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, -transform.up, out hit, 1))
+        {
+            return hit.collider.gameObject;
+        }
+        return null;
     }
 }
